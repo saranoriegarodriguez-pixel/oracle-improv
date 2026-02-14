7dbbc836497568409ea54f9ba64ad65b81079827
Sayan1563 <sara.noriega.rodriguez@gmail.com>
2026-02-14 20:16:12 +0100

    una barra

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AppSettingsProvider } from "./state/appSettings";
-import Topbar from "./components/Topbar"; // ✅ añade esto
 import "./index.css";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AppSettingsProvider>
-        <Topbar /> {/* ✅ Topbar global: portfolio + app */}
+
         <App />
       </AppSettingsProvider>
     </BrowserRouter>
